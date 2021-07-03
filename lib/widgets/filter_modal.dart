aa648b418005334d2bad0b1d9f7302f478b07c90
Kunal <kunal.bohra@somaiya.edu>
2021-07-03 22:15:56 +0530

    Added sliders in filter modal

--- lib/widgets/filter_modal.dart
+++ lib/widgets/filter_modal.dart
@@ -1,14 +1,12 @@
+import 'package:big_picture/widgets/filter_section.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import '../constants/styles.dart';
 import '../constants/strings.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class FilterModal extends StatelessWidget {
   final contentType;
   FilterModal({required this.contentType});
 
-  final _formKey = GlobalKey<FormBuilderState>();
 
   @override
   Widget build(BuildContext context) {
@@ -32,145 +30,7 @@ class FilterModal extends StatelessWidget {
             Container(
               height: size.height * 0.6 - 180,
               child: SingleChildScrollView(
-                child: FormBuilder(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      FormBuilderFilterChip(
-                        name: 'filter_chip',
-                        decoration: InputDecoration(
-                          labelText: 'Select many options',
-                        ),
-                        options: [
-                          FormBuilderFieldOption(
-                              value: 'Test', child: Text('Test')),
-                          FormBuilderFieldOption(
-                              value: 'Test 1', child: Text('Test 1')),
-                          FormBuilderFieldOption(
-                              value: 'Test 2', child: Text('Test 2')),
-                          FormBuilderFieldOption(
-                              value: 'Test 3', child: Text('Test 3')),
-                          FormBuilderFieldOption(
-                              value: 'Test 4', child: Text('Test 4')),
-                        ],
-                      ),
-                      FormBuilderChoiceChip(
-                        name: 'choice_chip',
-                        decoration: InputDecoration(
-                          labelText: 'Select an option',
-                        ),
-                        options: [
-                          FormBuilderFieldOption(
-                              value: 'Test', child: Text('Test')),
-                          FormBuilderFieldOption(
-                              value: 'Test 1', child: Text('Test 1')),
-                          FormBuilderFieldOption(
-                              value: 'Test 2', child: Text('Test 2')),
-                          FormBuilderFieldOption(
-                              value: 'Test 3', child: Text('Test 3')),
-                          FormBuilderFieldOption(
-                              value: 'Test 4', child: Text('Test 4')),
-                        ],
-                      ),
-                      FormBuilderDateTimePicker(
-                        name: 'date',
-                        // onChanged: _onChanged,
-                        inputType: InputType.time,
-                        decoration: InputDecoration(
-                          labelText: 'Appointment Time',
-                        ),
-                        initialTime: TimeOfDay(hour: 8, minute: 0),
-                        // initialValue: DateTime.now(),
-                        // enabled: true,
-                      ),
-                      FormBuilderDateRangePicker(
-                        name: 'date_range',
-                        firstDate: DateTime(1970),
-                        lastDate: DateTime(2030),
-                        format: DateFormat('yyyy-MM-dd'),
-                        onChanged: null,
-                        decoration: InputDecoration(
-                          labelText: 'Date Range',
-                          helperText: 'Helper text',
-                          hintText: 'Hint text',
-                        ),
-                      ),
-                      FormBuilderSlider(
-                        name: 'slider',
-                        validator: FormBuilderValidators.compose([
-                          FormBuilderValidators.min(context, 6),
-                        ]),
-                        onChanged: null,
-                        min: 0.0,
-                        max: 10.0,
-                        initialValue: 7.0,
-                        divisions: 20,
-                        activeColor: Colors.red,
-                        inactiveColor: Colors.pink[100],
-                        decoration: InputDecoration(
-                          labelText: 'Number of things',
-                        ),
-                      ),
-                      FormBuilderCheckbox(
-                        name: 'accept_terms',
-                        initialValue: false,
-                        onChanged: null,
-                        title: RichText(
-                          text: TextSpan(
-                            children: [
-                              TextSpan(
-                                text: 'I have read and agree to the ',
-                                style: TextStyle(color: Colors.black),
-                              ),
-                              TextSpan(
-                                text: 'Terms and Conditions',
-                                style: TextStyle(color: Colors.blue),
-                              ),
-                            ],
-                          ),
-                        ),
-                        validator: FormBuilderValidators.equal(
-                          context,
-                          true,
-                          errorText:
-                              'You must accept terms and conditions to continue',
-                        ),
-                      ),
-                      FormBuilderTextField(
-                        name: 'age',
-                        decoration: InputDecoration(
-                          labelText:
-                              'This value is passed along to the [Text.maxLines] attribute of the [Text] widget used to display the hint text.',
-                        ),
-                        onChanged: null,
-                        // valueTransformer: (text) => num.tryParse(text),
-                        validator: FormBuilderValidators.compose([
-                          FormBuilderValidators.required(context),
-                          FormBuilderValidators.numeric(context),
-                          FormBuilderValidators.max(context, 70),
-                        ]),
-                        keyboardType: TextInputType.number,
-                      ),
-                      FormBuilderDropdown(
-                        name: 'gender',
-                        decoration: InputDecoration(
-                          labelText: 'Gender',
-                        ),
-                        // initialValue: 'Male',
-                        allowClear: true,
-                        hint: Text('Select Gender'),
-                        validator: FormBuilderValidators.compose(
-                            [FormBuilderValidators.required(context)]),
-                        items: ['male', 'female']
-                            .map((gender) => DropdownMenuItem(
-                                  value: gender,
-                                  child: Text('$gender'),
-                                ))
-                            .toList(),
-                      ),
-                    ],
-                  ),
-                ),
+                child: FilterSection(),
               ),
             ),
             Divider(
---
+++ lib/widgets/filter_section.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class FilterSection extends StatefulWidget {
+  @override
+  _FilterSectionState createState() => _FilterSectionState();
+}
+
+enum YearChoice {
+  year,
+  range,
+  def,
+}
+
+class _FilterSectionState extends State<FilterSection> {
+  RangeValues _yearRangeSliderDiscreteValues = const RangeValues(1990, 2000);
+  RangeValues _runtimeRangeSliderDiscreteValues = const RangeValues(0, 120);
+  double _yearSliderDiscreteValue = 1990;
+  YearChoice? yearChoiceValue = YearChoice.def;
+
+  void onRadioSelected(YearChoice? yearChoice) {
+    setState(() {
+      yearChoiceValue = yearChoice;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          children: [
+            Radio(
+              value: YearChoice.year,
+              toggleable: true,
+              groupValue: yearChoiceValue,
+              activeColor: Color(0xFF6200EE),
+              onChanged: (YearChoice? yearChoice) =>
+                  onRadioSelected(yearChoice),
+            ),
+            Text('Select year'),
+            Expanded(
+              child: Text(
+                _yearSliderDiscreteValue.toInt().toString(),
+                textAlign: TextAlign.right,
+              ),
+            ),
+          ],
+        ),
+        Slider(
+          value: _yearSliderDiscreteValue,
+          min: 1900,
+          max: 2021,
+          label: _yearSliderDiscreteValue.round().toString(),
+          onChanged: yearChoiceValue != YearChoice.year
+              ? null
+              : (double value) {
+                  setState(() {
+                    _yearSliderDiscreteValue = value;
+                  });
+                },
+        ),
+        Text('OR'),
+        Row(
+          children: [
+            Radio(
+              value: YearChoice.range,
+              toggleable: true,
+              groupValue: yearChoiceValue,
+              activeColor: Color(0xFF6200EE),
+              onChanged: (YearChoice? yearChoice) =>
+                  onRadioSelected(yearChoice),
+            ),
+            Text('Select year range'),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    _yearRangeSliderDiscreteValues.start.toInt().toString(),
+                  ),
+                  Text('-'),
+                  Text(
+                    _yearRangeSliderDiscreteValues.end.toInt().toString(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        RangeSlider(
+          values: _yearRangeSliderDiscreteValues,
+          min: 1900,
+          max: 2021,
+          labels: RangeLabels(
+            _yearRangeSliderDiscreteValues.start.round().toString(),
+            _yearRangeSliderDiscreteValues.end.round().toString(),
+          ),
+          onChanged: yearChoiceValue != YearChoice.range
+              ? null
+              : (RangeValues values) {
+                  setState(() {
+                    _yearRangeSliderDiscreteValues = values;
+                  });
+                },
+        ),
+        Row(
+          children: [
+            Text('Select runtime range'),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    _runtimeRangeSliderDiscreteValues.start.toInt().toString(),
+                  ),
+                  Text('-'),
+                  Text(
+                    _runtimeRangeSliderDiscreteValues.end.toInt().toString(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        RangeSlider(
+          values: _runtimeRangeSliderDiscreteValues,
+          min: 0,
+          max: 240,
+          labels: RangeLabels(
+            _runtimeRangeSliderDiscreteValues.start.round().toString(),
+            _runtimeRangeSliderDiscreteValues.end.round().toString(),
+          ),
+          onChanged: (RangeValues values) {
+            setState(() {
+              _runtimeRangeSliderDiscreteValues = values;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
